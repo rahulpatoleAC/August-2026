e7c442dc00f4ca2bcfe4b3010c48b059252b9d25
rahulpatoleAC <rahul.patole.cmaug26@gmail.com>
2026-08-23 11:01:28 +0530

    Add files via upload

--- Assignments/logic building /Assignment 1/Problem_4.java
+++ Assignments/logic building /Assignment 1/Problem_4.java
@@ -1,4 +1,21 @@
-//Display good morning based on given time
+//Write a Java program that displays a "Good Morning" message if the
+//predefined time is between 5 AM and 12 PM. Use an if statement to implement the logic
 
+import java.util.Scanner;
 public class Problem_4 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your time");
+        int time = sc.nextInt();
+
+        if(time >= 5 && time < 12)
+        {
+            System.out.println("Good morning ");
+        }
+        else
+        {
+            System.out.println("Invalid Time ");
+        }
+
+    }
 }
